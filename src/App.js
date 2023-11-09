84154fbb59892ea681230856374727a17c775a24
Joseph Jou <josephjoufec@gmail.com>
2023-11-09 12:30:06 -0800

    connection to server apis

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import './App.css';
 import React from "react";
 import {Switch, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Nav, Navbar} from 'react-bootstrap';
+
 
 import AddReview from './components/add-review';
 import MovieList from './components/movies-list';
 import Movie from './components/movie';
 import Login from './components/login';
-import Navbar from './layouts/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar user={"John"}/>
-      Hello World!
+      <Navbar expand="lg" className="bg-body-tertiary">
+            <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link>
+                    <Link to={"/movies"}>Movies</Link>  
+                </Nav.Link>
+                <Nav.Link>
+                    { true?(<span>Logout User</span>):(<Link to = {"/login"}>Log In</Link>
+                    )}
+                    </Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+
     </div>
   );
 }
